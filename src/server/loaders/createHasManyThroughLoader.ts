17dcaac6aadfc7f48911382b4eb2ecdc2029edd5
Henrique <henrique@sidekick-coder.com>
2026-04-27 17:56:17 -0300

    fix: incorrect map keys

--- src/server/loaders/createHasManyThroughLoader.ts
+++ src/server/loaders/createHasManyThroughLoader.ts
@@ -1,6 +1,8 @@
 import { set, get } from 'lodash-es'
 import { defineLoader } from '#server/utils/defineLoader.ts'
 
+type KeyOrFunction<T> = keyof T | ((entity: T) => any)
+
 export interface HasManyThroughLoaderOptions<
     TEntity extends Record<string, any> = Record<string, any>,
     TPivotEntity extends Record<string, any> = Record<string, any>,
@@ -8,13 +10,13 @@ export interface HasManyThroughLoaderOptions<
 > {
     key: string // target property to set on the entity
     pivot: {
-        sourceKey: keyof TEntity // pivot table id key, default 'id'
-        targetKey: keyof TPivotEntity// pivot table foreign key to current entity
+        sourceKey: KeyOrFunction<TEntity> // pivot table foreign key to related entity
+        targetKey: KeyOrFunction<TPivotEntity> // pivot table foreign key to current entity
         findEntities(ids: (string | number)[]): Promise<TPivotEntity[]> // function to fetch pivot table rows by related entity ids
     },
     target: {
-        sourceKey: keyof TPivotEntity // target entity id key, default 'id'
-        targetKey: keyof TTargetEntity // target entity foreign key to pivot table
+        sourceKey: KeyOrFunction<TPivotEntity> // target entity id key, default 'id'
+        targetKey: KeyOrFunction<TTargetEntity> // target entity foreign key to pivot table
         findEntities(ids: (string | number)[]): Promise<TTargetEntity[]> // function to fetch related entities by ids
     }
 }
@@ -25,22 +27,32 @@ export async function loadHasManyThrough<
     TTargetEntity extends Record<string, any> = Record<string, any>
 >(payload: any | any[], options: HasManyThroughLoaderOptions<TEntity, TPivotEntity, TTargetEntity>) {
     const entities = Array.isArray(payload) ? payload : [payload]
+    const target = options.target 
+    const pivot = options.pivot
 
-    const pivoIds = entities.map(e => get(e, options.pivot.sourceKey)).filter(Boolean)
-    const pivotEntities = await options.pivot.findEntities(pivoIds)
-    const targetIds = pivotEntities.map((p: any) => get(p, options.target.sourceKey)).filter(Boolean)
-    const targetEntities = await options.target.findEntities(targetIds)
+    function findKey(entity: any, keyOrFn: KeyOrFunction<any>): any {
+        if (typeof keyOrFn === 'function') {
+            return keyOrFn(entity)
+        }
+
+        return get(entity, keyOrFn)
+        
+    }
+
+    const pivoIds = entities.map(e => findKey(e, pivot.sourceKey)).filter(Boolean)
+    const pivotEntities = await pivot.findEntities(pivoIds)
+    const targetIds = pivotEntities.map((p: any) => findKey(p, pivot.targetKey)).filter(Boolean)
+    const targetEntities = await target.findEntities(targetIds)
 
     for (const entity of entities) {
-        const pivots = pivotEntities.filter((p: any) => get(p, options.pivot.targetKey) === get(entity, options.pivot.sourceKey))
+        const pivots = pivotEntities.filter((p: any) => findKey(p, pivot.sourceKey) === findKey(entity, pivot.sourceKey))
 
         if (!pivots.length) {
             set(entity, options.key, [])
             continue
         }
 
-        const targets = targetEntities
-            .filter((t: any) => pivots.some((p: any) => get(p, options.target.targetKey) === get(t, options.target.sourceKey)))
+        const targets = targetEntities.filter((t: any) => pivots.some((p: any) => findKey(p, target.sourceKey) === findKey(t, target.targetKey)))
 
         set(entity, options.key, targets)
     }
