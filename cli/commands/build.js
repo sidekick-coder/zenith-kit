4545551121da248ea83b6ee347f850338b24fecb
Henrique <henrique@sidekick-coder.com>
2026-05-05 16:45:32 -0300

    feat: build server

--- cli/commands/build.js
+++ cli/commands/build.js
@@ -1,11 +1,21 @@
 import { Command } from 'commander'
+import { build } from 'tsdown'
+import createServerTsDownConfig from '../../extras/tsdown.js'
+import path from 'path'
 
 const command = new Command('build')
 
 command
     .description('Build plugin for production')
     .action(async () => {
-        console.log('Building plugin for production...')
+        const cwd = process.cwd()
+
+        const config = createServerTsDownConfig({
+            entry: path.resolve(cwd, 'src/server/index.ts'),
+            outDir: path.resolve(cwd, 'dist/server'),
+        })
+
+        await build(config)
     })
 
 export default command
---
+++ extras/tsdown.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'tsdown'
+import path from 'path'
+
+/**
+ * @typedef {Object} Options
+ * @property {string} [entry] - Entry file or directory (default: `src/server/index.ts`)
+ * @property {string} [outDir] - Output directory (default: `dist/server`)
+ */
+
+/**
+ * Vite plugin that replaces static and dynamic imports for specified modules
+ * with `globalThis.importAsync(...)` calls at transform time.
+ *
+ * @param {Options} options
+ */
+export default function createServerTsDownConfig(options) {
+    const tsConfigPath = path.resolve(import.meta.dirname, '..', 'tsconfig.server.json')
+
+    return defineConfig({
+        entry: options.entry,
+        outDir: options.outDir,
+        tsconfig: tsConfigPath,
+        minify: true,
+    })
+
+}
