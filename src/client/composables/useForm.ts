a8f225ca43dd9114d415e089853fa7aba7bcf13a
Henrique <henrique@sidekick-coder.com>
2026-09-22 16:07:17 -0300

    fix(client): use form input type wrong

--- src/client/composables/useForm.ts
+++ src/client/composables/useForm.ts
@@ -1,11 +1,11 @@
-import { type ValibotObjectSchema, type ValibotSchema, type ValidatePayload, type ValidatorResult } from '#shared/services/ValidatorService.ts';
-import { toTypedSchema as  veeValidateToTypedSchema } from '@vee-validate/valibot';
+import { type ValibotObjectSchema, type ValidatorResult } from '#shared/services/ValidatorService.ts';
+import * as v from 'valibot'
+import { toTypedSchema as veeValidateToTypedSchema } from '@vee-validate/valibot';
 import { useForm as useVeeValidateForm, type FormContext, type FormOptions, type GenericObject } from 'vee-validate';
 
 
-
-export interface ValibotFormOptions<T extends ValibotSchema = ValibotSchema> 
-    extends Omit<FormOptions<ValidatePayload<T>, ValidatorResult<T>>, 'validationSchema'> {}
+export interface ValibotFormOptions<T extends ValibotObjectSchema = ValibotObjectSchema> 
+    extends Omit<FormOptions<v.InferInput<T>, v.InferOutput<T>>, 'validationSchema'> {}
 
 export interface ValibotFormResultObject<T extends ValibotObjectSchema>
     extends GenericObject,
@@ -16,7 +16,7 @@ export const useForm = useValibotForm;
 
 export const toTypedSchema = veeValidateToTypedSchema;
 
-export function useValibotForm<T extends ValibotObjectSchema = ValibotObjectSchema>(schema: T, options: ValibotFormOptions<T> = {}): FormContext<ValidatorResult<T>> {
+export function useValibotForm<T extends ValibotObjectSchema = ValibotObjectSchema>(schema: T, options: ValibotFormOptions<T> = {}): FormContext<v.InferInput<T>, v.InferOutput<T>> {
     return useVeeValidateForm({
         ...options,
         validationSchema: veeValidateToTypedSchema(schema as any),
