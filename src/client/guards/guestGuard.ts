fbb5e48e37bdf6891ae59eb50d6e43e97850c83e
Henrique <henrique@sidekick-coder.com>
2026-09-23 18:47:04 -0300

    refactor(client): better redirect for guest guard

--- src/client/guards/guestGuard.ts
+++ src/client/guards/guestGuard.ts
@@ -1,19 +1,28 @@
 import auth from '#client/facades/auth.ts'
+import type { NavigationGuard } from 'vue-router'
 
 export interface GuestGuardOptions {
-    redirect: string
+    redirect?: string
 }
 
-export function createGuestGuard(options: GuestGuardOptions) {
-    return () => {
+export function createGuestGuard(options?: GuestGuardOptions): NavigationGuard {
+    return (to) => {
+        let redirect = to.query.redirect as string | undefined
+
+        if (!redirect) {
+            redirect = options?.redirect
+        }
+
+        if (!redirect) {
+            redirect = '/'
+        }
+
         if (auth.user) {
-            return options.redirect
+            return redirect
         }
     }
 }
 
-const guestGuard = createGuestGuard({
-    redirect: '/'
-})
+const guestGuard = createGuestGuard()
 
 export default guestGuard
